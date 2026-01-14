2ae500d3016d3c440362925e601718c8413304fe
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-01-14 20:43:42 +0800

    fix: add reload to run.py

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,9 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+    dev = os.environ.get("ENVIRONMENT") == "development"
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port,
+    )
